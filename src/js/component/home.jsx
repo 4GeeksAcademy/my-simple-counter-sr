c152237d59fec1c1f73c178c66d0b6ff52f093cf
Saray Rodríguez <rodriguezsaray602@gmail.com>
2025-01-20 22:19:39 +0000

    listoo :)

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faClock } from '@fortawesome/free-solid-svg-icons'; 
-library.add(faClock); 
+
 
 //create your first component
 const Home = (props) => {
